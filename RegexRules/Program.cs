f69ffae81021d314efdf87280b6738be4051d26d
Murugesan <Guhan.Murugesan@scientificgames.com>
2018-12-17 18:12:57 +0530

    regex expression updated

--- RegexRules/Program.cs
+++ RegexRules/Program.cs
@@ -13,6 +13,10 @@ namespace RegexRules
         {
             //(ILT_Gme)\:{1}\s(\w*)\s*(Var)\:*\s*(\d*)?\s?(Lev|Group|\$)?\:?\s?(\w[0x]?\d*\.?\d*)?\s?(\$|Lev)?\:?\s?(\d*\.?\d*)?\s?(\$)?(\d*\.?\d*)?
 
+            //(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+[0-9a-fA-F]{4})|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+[0-9a-fA-F]{4})|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+[0-9a-fA-F]{4})|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+[0-9a-fA-F]{4})|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+[0-9a-fA-F]{4})|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+[0-9a-fA-F]{4})|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+[0-9a-fA-F]{4})|(\d*\.*\d*)?))?\s*
+
+            //(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+\w\w\w\w)|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+\w\w\w\w)|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+\w\w\w\w)|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+\w\w\w\w)|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+\w\w\w\w)|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+\w\w\w\w)|(\d*\.*\d*)?))?\s*(?:(Lev|Var|Group|Denom|Payback|\$|ILT_Gme)\:?\s?(?:([0]+[xX]+\w\w\w\w)|(\d*\.*\d*)?))?\s*
+
             //ILT_Gme: 0x3ED0 Var: 99 $4500.48
             //ILT_Gme: 0x3ED0 Var: 99
             //ILT_Gme: 0x3ED0 Var: 99 Lev: 2 $32.24
